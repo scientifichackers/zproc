ae6565909e724f96258a231efdcbad562369a136
dev <devxpy@gmail.com>
2018-04-30 22:36:31 +0530

    bump version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/devxpy/zproc'
 EMAIL = 'devxpy@gmail.com'
 AUTHOR = 'Dev Aggarwal'
 REQUIRES_PYTHON = '>=3'
-VERSION = '0.1.6'
+VERSION = '0.1.7'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
