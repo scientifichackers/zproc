8845c66d4cc7b3ad44f0814549d3cb9e6efd2fd3
devxpy <devxpy@gmail.com>
2018-12-07 19:52:10 +0530

    add some commonly used atomic functions

--- zproc/__init__.py
+++ zproc/__init__.py
@@ -15,3 +15,18 @@ from .state.state import State, atomic
 from .task.result import SequenceTaskResult, SimpleTaskResult
 from .task.swarm import Swarm
 from .util import clean_process_tree, consume, create_ipc_address
+
+
+@atomic
+def increase(state, key, step=1):
+    state[key] += step
+
+
+@atomic
+def decrease(state, key, step=1):
+    state[key] -= step
+
+
+@atomic
+def append(state, key, item):
+    state[key].append(item)
